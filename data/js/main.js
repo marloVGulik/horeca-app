d9b42be4434375185e2814a04386fa2ba7362951
MarloVGulik <99060199@mydavinci.nl>
2019-10-08 10:16:10 +0200

    Added price feature
    added price features

--- data/js/main.js
+++ data/js/main.js
@@ -1,24 +1,30 @@
 var addNew = true;
 var orders = [];
+var totalPrice = 0.0;
 
 while(addNew) {
 	var added = prompt("Wat wilt U drinken/eten?");
 	if(added == "fris" || added == "Fris") {
-		orders[orders.length] = "Fris";
+		orders[orders.length] = "Fris, \u20AC1.50";
+		totalPrice += 1.50;
 		alert("Fris bij de bestelling gedaan");
 	} else if(added == "bier" || added == "Bier" || added == "pils" || added == "Pils") {
-		orders[orders.length] = "Bier";
+		orders[orders.length] = "Bier, \u20AC2.50";
+		totalPrice += 2.50;
 		alert("Bier bij de bestelling gedaan");
 	} else if(added == "wijn" || added == "Wijn") {
-		orders[orders.length] = "Wijn";
+		orders[orders.length] = "Wijn, \u20AC3.50";
+		totalPrice += 3.50;
 		alert("Wijn bij de bestelling gedaan");
-	} else if(added == "bitterballen" || added == "Bitterballen") {
+	} else if(added == "snack" || added == "Snack") {
 		var amount = prompt("Hoeveel bitterballen wilt U hebben? (8 of 16)");
 		if(amount == 8) {
-			orders[orders.length] = "8 bitterballen";
+			orders[orders.length] = "8 bitterballen, \u20AC1.50";
+			totalPrice += 1.50;
 			alert("8 bitterballen bij de bestelling gedaan");
 		} else if(amount == 16) {
-			orders[orders.length] = "16 bitterballen";
+			orders[orders.length] = "16 bitterballen, \u20AC2.50";
+			totalPrice += 2.50;
 			alert("16 bitterballen bij de bestelling gedaan");
 		} else {
 			alert("Geen bitterballen bij de bestelling gedaan omdat je geen 8 of 16 heb ingevoerd");
@@ -37,3 +43,5 @@ orders.forEach(function(item) {
 	listItem.appendChild(document.createTextNode(item));
 	document.getElementById("List").appendChild(listItem);
 });
+
+document.getElementById("price").innerHTML = "\u20AC" + totalPrice;
